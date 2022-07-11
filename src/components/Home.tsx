6b1c89e52050cdc096bb12eb44c391cb0f9444f1
Shahzaib-ur-Rehman <shaahzaibrehman@gmail.com>
2022-07-11 11:42:27 +0500

    incomplete project

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,10 +3,18 @@ import Footer from "./Footer";
 import ConnectWallet from "./ConnectWallet";
 import Banner from "./Banner";
 import BuyCake from "./BuyCake";
+import Trade from "./Trade";
+import TrustedUsers from "./TrustedUsers";
+import WinPrices from "./WinPrices";
+import EarnPassiveEncome from "./EarnPassiveEncome";
 export default function Home() {
   return (
     <div>
       <Banner />
+      <TrustedUsers />
+      <Trade />
+      <EarnPassiveEncome />
+      <WinPrices />
       <BuyCake />
       <ConnectWallet />
       <Footer />
